6193e5554c1bb33b24d3b8e99947c674246eedd8
Ajit Dhivar <ajitdhivar4d@gmail.com>
2024-08-03 19:26:19 +0530

    cart component update

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,46 +1,37 @@
 import Navbar from "../components/Navbar";
 
-const img1 =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwPfCnG51iZgO-2KtE8Psyn2W2g0wTzgWJxA&s";
-const img2 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Pizza_im_Pizzaofen_von_Maurizio.jpg/544px-Pizza_im_Pizzaofen_von_Maurizio.jpg";
-
 const Cart = () => {
+  const deleteHandler = () => {
+    console.log("Delete");
+  };
   return (
     <div className="cart-container">
       <Navbar />
-
-      <div className="cart">
-        <p>Date - 26 march 2023</p>
-        <hr />
-        <section>
-          <div className="cart-item">
-            <img src={img1} alt="image" />
-            <h3>Pizza </h3>
-            <span>$250</span>
-          </div>
-          <div className="cart-item">
-            <img src={img2} alt="image" />
-            <h3>Pizza </h3>
-            <span>$250</span>
-          </div>
-        </section>
-      </div>
-      <div className="cart">
-        <p>Date - 26 march 2023</p>
-        <hr />
-        <section>
-          <div className="cart-item">
-            <img src={img1} alt="image" />
-            <h3>Pizza </h3>
-            <span>$250</span>
-          </div>
-          <div className="cart-item">
-            <img src={img2} alt="image" />
-            <h3>Pizza </h3>
-            <span>$250</span>
-          </div>
-        </section>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Quantity</th>
+              <th>Option</th>
+              <th>Amount</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Iphone</td>
+              <td>2</td>
+              <td>pro modal</td>
+              <td>45000</td>
+              <td>
+                <button onClick={deleteHandler}>Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
--- src/pages/MyOrder.tsx
+++ src/pages/MyOrder.tsx
@@ -1,5 +1,49 @@
+import Navbar from "../components/Navbar";
+
+const img1 =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwPfCnG51iZgO-2KtE8Psyn2W2g0wTzgWJxA&s";
+const img2 =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Pizza_im_Pizzaofen_von_Maurizio.jpg/544px-Pizza_im_Pizzaofen_von_Maurizio.jpg";
+
 const MyOrder = () => {
-  return <div>MyOrder</div>;
+  return (
+    <div className="myOrder-container">
+      <Navbar />
+
+      <div className="myOrder">
+        <p>Date - 26 march 2023</p>
+        <hr />
+        <section>
+          <div className="myOrder-item">
+            <img src={img1} alt="image" />
+            <h3>Pizza </h3>
+            <span>$250</span>
+          </div>
+          <div className="myOrder-item">
+            <img src={img2} alt="image" />
+            <h3>Pizza </h3>
+            <span>$250</span>
+          </div>
+        </section>
+      </div>
+      <div className="myOrder">
+        <p>Date - 26 march 2023</p>
+        <hr />
+        <section>
+          <div className="myOrder-item">
+            <img src={img1} alt="image" />
+            <h3>Pizza </h3>
+            <span>$250</span>
+          </div>
+          <div className="myOrder-item">
+            <img src={img2} alt="image" />
+            <h3>Pizza </h3>
+            <span>$250</span>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default MyOrder;
